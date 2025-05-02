b81086116f3711ff450f4650c5c0bbacdb744808
ericcfrancis <ericcire49@gmail.com>
2025-05-02 15:57:16 +0800

    add mcoisop route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MsicComponent } from './msic/msic.component';
+import { McoicopComponent } from './mcoicop/mcoicop.component';
 
 export const routes: Routes = [
     { path: 'msic', component: MsicComponent},
+    { path: 'mcoicop', component: McoicopComponent },
     {path: '', redirectTo: 'msic', pathMatch: 'full'},
 ];
