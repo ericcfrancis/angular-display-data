d80b63167354b474df2f720ba94cf7060822aa9e
ericcfrancis <ericcire49@gmail.com>
2025-05-02 15:29:39 +0800

    configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MsicComponent } from './msic/msic.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'msic', component: MsicComponent},
+    {path: '', redirectTo: 'msic', pathMatch: 'full'},
+];
